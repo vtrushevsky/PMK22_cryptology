591abd03a76de8eab0e43a20a93f07db10b8b5a6
KrissSab <116729769+KrissSab@users.noreply.github.com>
2023-06-04 17:19:09 +0300

    formatTetx

---
+++ formatText.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <windows.h>
+#include <ctime>
+#include <sstream>
+using namespace std;
+
+ostream& skipCharacterByValue(ostream& os, int value)
+{
+    for (int i = 0; i < value; i++)
+    {
+        os << " ";
+    }
+    return os;
+}
+void printLogarithm()
+{
+    skipCharacterByValue(cout, 90);
+    cout << "Nat. Log" << " ";
+    cout << "10t. Log" << endl;
+    skipCharacterByValue(cout, 90);
+    cout << "-------------------------" << endl;
+    for (int i = 2; i < 100; i++)
+    {
+        skipCharacterByValue(cout, 90);
+        cout << "|    " << setprecision(5) << log(i) << "  |";
+        cout << setprecision(5) << log10(i) << "    |" << endl;
+        skipCharacterByValue(cout, 90);
+        cout << "-------------------------" << endl;
+    }
+}
+
+void printCentered()
+{
+    wstring text = L"";
+    getline(wcin, text);
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(consoleHandle, &consoleInfo);
+    int consoleWidth = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
+    int consoleHeight = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1;
+
+    COORD cursorPos;
+    cursorPos.X = (consoleWidth - text.length()) / 2;
+    cursorPos.Y = consoleHeight / 2;
+    SetConsoleCursorPosition(consoleHandle, cursorPos);
+
+    wcout << text << endl;
+}
+
+wostream& systemTimeAndDate(wostream& os)
+{
+    time_t currentTime;
+    tm localTime;
+    time(&currentTime);
+
+    localtime_s(&localTime, &currentTime);
+
+    wstringstream wss;
+    wss << L"Current system time and date: " << put_time(&localTime, L"%Y-%m-%d %H:%M:%S");
+
+    os << wss.str();
+
+    return os;
+}
+
+istream& skipCharacters(istream& is)
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        if (!is.get())
+            break;
+    }
+    return is;
+}
+int main()
+{   
+    return 0;
+}
